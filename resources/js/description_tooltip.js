9b3d8322e6cf5105e6ea68460284d14da2174b95
zyyx-nghiann <nghiann@zyyx.jp>
2023-04-16 15:26:15 +0700

    feat: Main page template

---
+++ resources/js/description_tooltip.js
@@ -0,0 +1,39 @@
+let description_tooltip = $('.description_tooltip')
+
+document.addEventListener(
+    'mousemove',
+    function (e) {
+
+        let client_screen = $(document).width()
+
+        for (let i = description_tooltip.length; i--;) {
+            let description_ele = $(description_tooltip[i])
+            if (e.pageX >= (client_screen / 2)) {
+                description_ele.css('right', (client_screen - e.pageX) + 'px')
+                description_ele.css('top', e.pageY + 'px')
+                description_ele.css('left', '')
+                description_ele.css('margin-right', '1rem')
+                description_ele.css('margin-left', '')
+            } else {
+                description_ele.css('left', e.pageX + 'px')
+                description_ele.css('top', e.pageY + 'px')
+                description_ele.css('right', '')
+                description_ele.css('margin-left', '1rem')
+                description_ele.css('margin-right', '')
+            }
+        }
+    },
+    false
+)
+
+let title_manga_card = $('.title_manga_card')
+
+title_manga_card.hover(
+    function () {
+        description_tooltip.css('display', 'block')
+    },
+
+    function () {
+        description_tooltip.css('display', 'none')
+    }
+)
