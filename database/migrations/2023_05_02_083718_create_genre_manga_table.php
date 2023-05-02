a05d11bd0925b8056f8b63cdd309bbd4ff3a5e57
cungaminoan <cungaminoan@gmail.com>
2023-05-02 16:35:50 +0700

    feat: DungTT migration

---
+++ database/migrations/2023_05_02_083718_create_genre_manga_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGenreMangaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genre_manga', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('genre_id');
+            $table->tinyInteger('manga_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genre_manga');
+    }
+}
