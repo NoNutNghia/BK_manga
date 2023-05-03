f3419648a2d851a38915c2970b72566358735cac
Minh <theonething2001@gmail.com>
2023-05-03 13:15:27 +0700

    'pushh'

---
+++ database/migrations/2023_05_03_051921_create_chapter_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChapterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapter', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('manga_id');
+            $table->integer('uploaded_by');
+            $table->timestamp('uploaded_at');
+            $table->integer('updated_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapter');
+    }
+}
