951ab31341c1bd18b1209248f66f99e6c1c0064f
Minh <theonething2001@gmail.com>
2023-05-08 14:09:58 +0700

    commit

---
+++ app/Models/ChapterComment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChapterComment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'chapter_id',
+        'user_id',
+        'content'
+       
+    ];
+
+    protected $table = 'chapter_comment';
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+
+
+    public $timestamps = true;
+}
---
+++ app/Models/MangaComment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MangaComment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'manga_id',
+        'content'
+       
+    ];
+
+    protected $table = 'manga_comment';
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+
+
+    public $timestamps = true;
+}
